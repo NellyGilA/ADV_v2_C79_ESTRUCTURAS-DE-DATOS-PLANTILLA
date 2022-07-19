aa4bd7296073e45b3e4d9363e5b4c0e8a0540a39
NellyGilA <86023328+NellyGilA@users.noreply.github.com>
2022-07-19 00:21:43 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,29 @@
-
-	nombre_alumno_array = [];
-	
-	function submit()
-	{
+nombre_alumno_array = [];
         
-        var nombre_1 = document.getElementById("nombre_alumno_1").value;
-        var nombre_2 = document.getElementById("nombre_alumno_2").value;
-        
+function submit()
+{
+            
+    var nombre_1 = document.getElementById("nombre_alumno_1").value;
+    var nombre_2 = document.getElementById("nombre_alumno_2").value;
+    var nombre_3 = document.getElementById("nombre_alumno_3").value;
+    //var nombre_4 = document.getElementById("nombre_alumno_4").value;
 
-        nombre_alumno_array.push(nombre_1);
-        nombre_alumno_array.push(nombre_2);
-       
+    nombre_alumno_array.push(nombre_1);
+    nombre_alumno_array.push(nombre_2);
+    nombre_alumno_array.push(nombre_3);
+    //
 
-		console.log();
-		
-        //document.getElementById("ver_nombre").innerHTML = nombre_alumno_array;
-        //document.getElementById("botón_enviar").style.display = "none";
-        //document.getElementById("botón_ordenar").style.display = "inline-block";
+    console.log(nombre_alumno_array);
+            
+    //document.getElementById("ver_nombre").innerHTML = nombre_alumno_array;
+    //document.getElementById("botón_enviar").style.display = "none";
+    //document.getElementById("botón_ordenar").style.display = "inline-block";
 
-	}
+}
 
 function sorting()
 {
-	
+	//
+    console.log(nombre_alumno_array);
+	//
 }
